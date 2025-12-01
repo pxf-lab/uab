67a72de0773bf44bd047fe9ec36ed360922fcb28
Ben Cressman <ben@utdallas.edu>
2025-12-01 08:16:15 -0600

    feat(Asset): add dictionary conversion methods

--- src/uab/core/assets.py
+++ src/uab/core/assets.py
@@ -30,6 +30,44 @@ class Asset(ABC):
         self.date_created = date_created
         self.date_added = date_added
 
+    @classmethod
+    def from_dict(cls, data: dict) -> "Asset":
+        """
+        Create an Asset from a plain dictionary coming from the API / DB.
+
+        Expects keys compatible with the backend model / API schema.
+        """
+        if data is None:
+            raise ValueError("Cannot create Asset from None")
+
+        return cls(
+            name=data.get("name", ""),
+            path=data.get("path", ""),
+            asset_id=data.get("id"),
+            description=data.get("description"),
+            preview_image_file_path=data.get("preview_image_file_path"),
+            tags=data.get("tags") or [],
+            author=data.get("author"),
+            date_created=data.get("date_created"),
+            date_added=data.get("date_added"),
+        )
+
+    def to_dict(self) -> dict:
+        """
+        Convert this Asset into a serialisable dictionary suitable for API calls.
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "path": self.path,
+            "preview_image_file_path": self.preview_image_file_path,
+            "tags": list(self.tags) if self.tags is not None else [],
+            "author": self.author,
+            "date_created": self.date_created,
+            "date_added": self.date_added,
+        }
+
 
 class Texture(Asset):
     def __init__(self, name: str, path: str, color_space: str):
