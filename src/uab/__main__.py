0b1d4cd4fc68d80752d78f9a697ec8300c6087c2
Ben Cressman <ben@utdallas.edu>
2026-01-07 19:20:44 -0600

    feat: init uv project

--- src/uab/__main__.py
+++
@@ -1,5 +0,0 @@
-import sys
-from uab.runner import run
-
-if __name__ == "__main__":
-    sys.exit(run())
---
+++ src/uab/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from uab!")
+
+
+if __name__ == "__main__":
+    main()
